4e14d09dbde985e70783c9db2e08f7973513848b
bmasias <basti14.masias@gmail.com>
2021-09-01 16:48:33 -0400

    perfil para subir publicaciones

---
+++ Controlador/ADM/navegacion.php
@@ -0,0 +1,72 @@
+<!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Agrofaenas <sup></sup></div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRecepcion" aria-expanded="true" aria-controls="collapseDespacho">
+          <i class="fas fa-briefcase"></i>
+          <span>REPORTES</span>
+        </a>
+        <div id="collapseRecepcion" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">FUNCIONES:</h6>
+            <a class="collapse-item" href="listaRecepcionDiaria.php">Bodega de Recepción </a>
+            <a class="collapse-item" href="listaRecepciones.php">Historial Recepción</a>
+            
+          </div>
+        </div>
+      </li>
+
+
+
+ 
+
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        MAESTROS
+      </div>
+
+
+
+      <li class="nav-item">
+        <a class="nav-link" href="listaCCosto.php">
+          <i class="fas fa-fw fa-wrench"></i>
+          <span>Centro Costo</span></a>
+      </li>
+      
+
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!-- End of Sidebar -->
